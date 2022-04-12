9567415fb0f82f20cb48eb65f706dae854c569c0
jasin-ska <lenajas0110@gmail.com>
2022-04-12 04:51:36 +0200

    Poprawki

--- Lista 4/zad1.pl
+++ Lista 4/zad1.pl
@@ -1,11 +1,12 @@
+%wyra¿enie2([], 0, 1).
+
 wyra¿enie2([X], X, X).
 
-%wyra¿enie2([X | T], Y, W) :-
-%    wyra¿enie2(T, Y1, W1),
-%    ((   W = X * W1, Y is X * Y1);
-%    (   W = X - W1, Y is X - Y1);
-%    (   W = X + W1, Y is X + Y1);
-%    (   Y1 =\= 0, W = X / W1, Y is X / Y1)).
+%wyra¿enie2([X, Y], Z, W) :-
+ %    ((Z is X * Y, W = X * Y);
+  %   (Z is X - Y, W = X - Y);
+   %  (Z is X + Y, W = X + Y);
+    % (Y =\= 0, Z is X / Y, W = X / Y)).
 
 wyra¿enie2(L, Y, W) :-
     append(L1, L2, L),
@@ -13,10 +14,10 @@ wyra¿enie2(L, Y, W) :-
     length(L2, Le2), Le2 > 0,
     wyra¿enie2(L1, Y1, W1),
     wyra¿enie2(L2, Y2, W2),
-    ((   W = W1 * W2, Y is Y1 * Y2);
-    (   W = W1 - W2, Y is Y1 - Y2);
-    (   W = W1 + W2, Y is Y1 + Y2);
-    (   Y2 =\= 0, W = W1 / W2, Y is Y1 / Y2)).
+       ((Y is Y1 * Y2, W = W1 * W2);
+       (Y is Y1 - Y2, W = W1 - W2);
+       (Y is Y1 + Y2, W = W1 + W2);
+       (Y2 =\= 0, Y is Y1 / Y2, W = W1 / W2)).
 
 
 wyra¿enie(L, X, W) :- distinct(wyra¿enie2(L, X, W)).
--- Lista 4/zad3.pl
+++ Lista 4/zad3.pl
@@ -6,6 +6,13 @@ small(L) :-
 	W is N + 3, nth1(3, L, W),
 	E is N + 4, nth1(4, L, E).
 
+small(L, N) :-
+	N < 17,
+	N mod 7 < 3, member(N, L),
+	S is N + 7, member(S, L), S < 24,
+	W is N + 3, member(W, L),
+	E is N + 4, member(E, L).
+
 medium(L) :-
 	L = [0, 1, 14, 15, 3, 10, 5, 12];
 	L = [1, 2, 15, 16, 4, 11, 6, 13];
@@ -16,47 +23,56 @@ big(L) :-
 	L = [0, 1, 2, 3, 6, 10, 13, 17, 20, 21, 22, 23], !.
 
 
-zapa³ki(K, B, M, S) :- % Big, Medium, Small
-	between(0, 1, B),
-	between(0, 4, M),
-	between(0, 9, S),
-	between(0, 24, K),
-        (B = 1 -> big(Set) ; Set = []),
-	add2(Set, M, [], Set2),
-	%length(Mediums, M),
-	%foreach(member(Mediums, X), medium(X)),
-	%is_set(Mediums),
-	%add(1, Mediums, M, Set, Set2),
-	draw(Set2).
+getSet(K, B, M, S, SetOut) :- % Big, Medium, Small
+	B>=0, B=<1,
+	M>=0, M=<4,
+	S>=0, S=<9,
+        (B = 1 -> big(Set) ; Set = []), !,
+	distinct(add2(Set, M, [], Set2)),
+	%draw(Set2),
+	countSmalls(Set2, SmallNr),
+	SmallNr =< S,
+	S2 is S - SmallNr,
+	add3(Set2, S2, Set2, Set3, SmallNr),
+	length(Set3, LenSet),
+        K is 24 - LenSet,
+	sort(Set3, SetOut).
 
-%zapa³ki(K, B, M, S, []).
+zapa³ki(K, B, M, S) :-
+	distinct(getSet(K, B, M, S, Set)),
+	draw(Set).
 
 add2(Set, 0, Mediums, SetOut) :-
-	union(Set, Mediums, SetOut).
+	union(Set, Mediums, Set2),
+	sort(Set2, SetOut).
 
 add2(Set, M, Mediums, SetOut) :-
 	medium(X),
-	intersection(X, Mediums, Inter),
-	\+(Inter = X), % juz dodany
+	\+subset(X, Mediums),
 	union(Mediums, X, Mediums2),
 	M2 is M - 1,
 	add2(Set, M2, Mediums2, SetOut).
 
+add3(Set, 0, Smalls, SetOut, _) :-
+	union(Set, Smalls, SetOut).
 
-add(Id, Squares, Length, Set, SetOut) :-
-	(Id > Length -> SetOut = Set; true),
-	nth1(Id, Squares, X),
-	union(X, Set, Set2),
-	Id2 is Id + 1,
-	add(Id2, Squares, Length, Set2, SetOut).
-
-checkSmalls(Set) :-
-	Smalls = [].
-	%foreach(member(Set, X).
+add3(Set, S, Smalls, SetOut, NowSmalls) :-
+	small(X),
+	\+subset(X, Smalls),
+	union(Smalls, X, Smalls2),
+	countSmalls(Smalls2, NowSmalls2),
+	R is NowSmalls2 - NowSmalls,
+	S2 is S- R,
+	add3(Set, S2, Smalls2, SetOut, NowSmalls2).
 
 
-%zapa³ki(K, B, M, S, Set) :-
-%	(B = 1 -> union())
+countSmalls(L, Count) :-
+	findall(X, check(L, X), Smalls),
+	length(Smalls, Count).
+
+check(L, X) :-
+	small(X),
+	subset(X, L).
 
 
 
@@ -79,18 +95,4 @@ mhe() :- write("+   ").
 mv() :- write("|     ").
 mve() :- write("      ").
 
-
-%
-%medium(L) :-
-%	length(L, 8),
-%	between(0, 23, N1),
-%	N1 mod 7 < 2, nth1(1, L, N1),
-%	N2 is N1 + 1, nth1(2, L, N2),
-%	S1 is N1 + 14, nth1(3, L, S1),
-%	S2 is N1 + 15, nth1(4, L, S2),
-%	W1 is N1 + 3, nth1(5, L, W1),
-%	W2 is N1 + 10, nth1(6, L, W2),
-%	E1 is N2 + 4, nth1(7, L, E1),
-%	E2 is N2 + 11, nth1(8, L, E2),
-%	foreach(member(X, L), between(0, 23, X)).
 
